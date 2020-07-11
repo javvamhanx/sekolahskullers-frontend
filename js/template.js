e9014a19c4892d08c108e6450f936ca074a81d0d
salehzaidan <salehzaidan53@gmail.com>
2020-07-11 13:34:59 +0700

    Menambahkan nilai mingguan ke halaman nilai
    Mengubah script template.js dengan jQuery

--- js/template.js
+++ js/template.js
@@ -1,9 +1,5 @@
-navItems = document.querySelectorAll(".nav-item");
-navItems.forEach((navItem) => {
-  navItem.addEventListener("click", () => {
-    navItems.forEach((element) => {
-      element.classList.remove("active");
-    });
-    navItem.classList.add("active");
-  });
+$('.nav-item').click(function (e) { 
+  e.preventDefault();
+  $('.nav-item').removeClass('active');
+  $(this).addClass('active');
 });
